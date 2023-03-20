6a1759996801496e1be61458e719e8e3970ca098
gedeapriana <gedeapriana36@gmail.com>
2023-03-21 01:02:55 +0800

    tasks design

--- pemrograman-berbasis-web-dashboard/src/components/task/TaskAccordion.jsx
+++ pemrograman-berbasis-web-dashboard/src/components/task/TaskAccordion.jsx
@@ -1,86 +1,57 @@
-import React from "react";
-import { Accordion } from "flowbite-react";
+import React, { useEffect } from "react";
+import { Accordion, Badge, Button } from "flowbite-react";
+import Tasks from './Data.json';
 
 const TaskAccordion = () => {
+
+
   return (
     <>
-      <Accordion class="w-full" alwaysOpen={true}>
-  <Accordion.Panel>
-    <Accordion.Title>
-      What is Flowbite?
-    </Accordion.Title>
-    <Accordion.Content>
-      <p className="mb-2 text-gray-500 dark:text-gray-400">
-        Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more.
-      </p>
-      <p className="text-gray-500 dark:text-gray-400">
-        Check out this guide to learn how to
-        <a
-          href="https://flowbite.com/docs/getting-started/introduction/"
-          className="text-blue-600 hover:underline dark:text-blue-500"
-        >
-          get started
-        </a>
-         and start developing websites even faster with components on top of Tailwind CSS.
-      </p>
-    </Accordion.Content>
-  </Accordion.Panel>
-  <Accordion.Panel>
-    <Accordion.Title>
-      Is there a Figma file available?
-    </Accordion.Title>
-    <Accordion.Content>
-      <p className="mb-2 text-gray-500 dark:text-gray-400">
-        Flowbite is first conceptualized and designed using the Figma software so everything you see in the library has a design equivalent in our Figma file.
-      </p>
-      <p className="text-gray-500 dark:text-gray-400">
-        Check out the
-        <a
-          href="https://flowbite.com/figma/"
-          className="text-blue-600 hover:underline dark:text-blue-500"
-        >
-          Figma design system
-        </a>
-         based on the utility classes from Tailwind CSS and components from Flowbite.
-      </p>
-    </Accordion.Content>
-  </Accordion.Panel>
-  <Accordion.Panel>
-    <Accordion.Title>
-      What are the differences between Flowbite and Tailwind UI?
-    </Accordion.Title>
-    <Accordion.Content>
-      <p className="mb-2 text-gray-500 dark:text-gray-400">
-        The main difference is that the core components from Flowbite are open source under the MIT license, whereas Tailwind UI is a paid product. Another difference is that Flowbite relies on smaller and standalone components, whereas Tailwind UI offers sections of pages.
-      </p>
-      <p className="mb-2 text-gray-500 dark:text-gray-400">
-        However, we actually recommend using both Flowbite, Flowbite Pro, and even Tailwind UI as there is no technical reason stopping you from using the best of two worlds.
-      </p>
-      <p className="mb-2 text-gray-500 dark:text-gray-400">
-        Learn more about these technologies:
-      </p>
-      <ul className="list-disc pl-5 text-gray-500 dark:text-gray-400">
-        <li>
-          <a
-            href="https://flowbite.com/pro/"
-            className="text-blue-600 hover:underline dark:text-blue-500"
-          >
-            Flowbite Pro
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://tailwindui.com/"
-            rel="nofollow"
-            className="text-blue-600 hover:underline dark:text-blue-500"
-          >
-            Tailwind UI
-          </a>
-        </li>
-      </ul>
-    </Accordion.Content>
-  </Accordion.Panel>
-</Accordion>
+      <Accordion class="w-full" alwaysOpen={true} flush={false}>
+        {
+          Tasks.map((task) => {
+            return (
+              <Accordion.Panel>
+                <Accordion.Title>
+                  {task.title}
+                </Accordion.Title>
+                <Accordion.Content >
+                  <section className="flex items-center justify-start gap-3 flex-wrap">
+                    {task.status === "Proses" ? <div className="flex flex-wrap gap-2"><Badge color="warning">Status {task.status}</Badge></div> : task.status === "Selesai" ? <div className="flex flex-wrap gap-2"><Badge color="success">Status {task.status}</Badge></div> : <div className="flex flex-wrap gap-2"><Badge color="failure">Status {task.status}</Badge></div> }
+
+                    {task.deadline !== "" ? <div className="flex flex-wrap gap-2"><Badge color="failure">Deadline {task.deadline}</Badge></div> : ""}
+
+                  </section>
+                  <section className="w-full p-3 bg-violet-100 dark:bg-slate-800 my-3 rounded-lg">
+                    <p className="text-violet-500 text-xs">Detail tugas</p>
+                    <p className="text-sm dark:text-slate-50 text-slate-700 pt-3 py-1">{task.description.detail}</p>
+                    {task.description.ketentuan !== "" ? <ul className="list-disc mx-5 text-xs text-slate-700 dark:text-slate-300"><li style={{fontFamily: "qsand"}}>{task.description.ketentuan}</li></ul> : ""}
+                  </section>
+                  <section className="w-full flex gap-5 justify-start items-center flex-wrap">
+                    <Button.Group>
+                      <Button color="gray" href="https://www.google.com" target="_blank">
+                        Link Pengumpulan
+                      </Button>
+                      <Button color="gray">
+                        Template Laporan
+                      </Button>
+                    </Button.Group>
+
+                    {
+                      task.status === "Selesai" ? 
+                        <a href={task.result} target="_blank">
+                          <Button class="bg-violet-400 text-slate-100 rounded-lg">
+                            Success
+                          </Button>
+                        </a> : ""
+                    }
+                  </section>
+                </Accordion.Content>
+              </Accordion.Panel>
+            )
+        })
+        }
+      </Accordion>
     </>
   )
 }
